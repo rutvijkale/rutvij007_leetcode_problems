f32176a3e21183381e71a572abbf1d7c9fd7eaef
rutvijkale <rutvijgvkale@gmail.com>
2026-08-29 11:39:05 +0530

    Time: 0 ms (100%), Space: 8.7 MB (89.06%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool isValid(string s) 
+    {
+       stack<char> st;
+       for(int i=0;i<s.size();i++)
+       {
+        if(s[i]=='{'||s[i]=='['||s[i]=='(')
+        {
+            st.push(s[i]);
+        }
+       else
+        {
+            if(st.empty())return false;
+            if(s[i]==')')
+            {if(s[i]==')'&&st.top()!='(')return false;
+            else{
+                st.pop();
+            }}
+
+           else if(s[i]==']'){ if(s[i]==']'&&st.top()!='[')return false;
+            else{
+                st.pop();
+            }}
+
+            else if(s[i]=='}')
+           { if(s[i]=='}'&&st.top()!='{')return false;
+            else{
+               st.pop();
+            }}
+            else{return false;}
+        }
+       }
+       if(!st.empty())return false;
+       return true;
+    }
+};
