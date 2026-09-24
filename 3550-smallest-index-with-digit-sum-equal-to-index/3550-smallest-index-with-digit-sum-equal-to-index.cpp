f724423c742e5246e37a1d1d21ae427bd50a0f3b
rutvijkale <rutvijgvkale@gmail.com>
2026-09-24 09:57:58 +0530

    Time: 0 ms (100%), Space: 30.9 MB (52.36%) - LeetHub

--- 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.cpp
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.cpp
@@ -4,12 +4,16 @@ public:
     for(int i=0;i<nums.size();i++)
     {
         int sum=0;
-        while(nums[i]>0)
+        if(nums[i]<=9)
+        {
+            if(nums[i]==i)return i;
+        }
+       else{ while(nums[i]>0)
         {
             sum+=nums[i]%10;
             nums[i]=nums[i]/10;
         }
-        if(sum==i)return i;
+        if(sum==i)return i;}
     }
     return -1;
     }
